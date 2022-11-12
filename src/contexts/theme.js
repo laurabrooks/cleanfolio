fabb59a4a3c3a999e3d1f3709bdd4c35853f11a3
Laura Brooks <itslaurabrooks@gmail.com>
2022-11-11 21:28:24 -0500

    persist toggled theme preference

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -4,19 +4,30 @@ import PropTypes from 'prop-types'
 const ThemeContext = createContext()
 
 const ThemeProvider = ({ children }) => {
-  const [themeName, setThemeName] = useState('light')
+  const themeEnums = Object.freeze({
+    light: 'light',
+    dark: 'dark',
+    themeName: 'themeName',
+  });
+  
+  const [themeName, setThemeName] = useState(themeEnums.light)
 
   useEffect(() => {
-    const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    setThemeName(darkMediaQuery.matches ? 'dark' : 'light')
-    darkMediaQuery.addEventListener('change', (e) => {
-      setThemeName(e.matches ? 'dark' : 'light')
-    });
+    if (!localStorage.getItem(themeEnums.themeName)) {
+      const darkMediaQuery = window.matchMedia(`(prefers-color-scheme: ${themeEnums.dark})`);
+      setThemeName(darkMediaQuery.matches ? themeEnums.dark : themeEnums.light)
+      darkMediaQuery.addEventListener('change', (e) => {
+        setThemeName(e.matches ? themeEnums.dark : themeEnums.light)
+      });
+    } else {
+      // if a preference is in localStorage already, use that
+      setThemeName(localStorage.getItem(themeEnums.themeName) === themeEnums.dark ? themeEnums.dark : themeEnums.light);
+    }
   }, [])
 
   const toggleTheme = () => {
-    const name = themeName === 'dark' ? 'light' : 'dark'
-    localStorage.setItem('themeName', name)
+    const name = themeName === themeEnums.dark ? themeEnums.light : themeEnums.dark
+    localStorage.setItem(themeEnums.themeName, name)
     setThemeName(name)
   }
 
